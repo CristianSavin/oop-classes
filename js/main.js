9517d9ed69e2e70c0c63e5ba40ab54835193093c
Cristian Savin <savin.cristian95@icloud.com>
2017-11-26 21:34:36 +0200

    Commit 8

--- js/main.js
+++ js/main.js
@@ -1,122 +1,113 @@
-// /* CLASSES IN JAVASCRIPT EXAMPLE USE */
-// class user{
-//     constructor(f_name , l_name , password, email , phone_number){
-//         this.f_name = f_name;
-//         this.l_name = l_name;
-//         this.password = password;
-//         this.email = email;
-//         this.phone_number = phone_number;
-//         this.avatar = "http://wicklownews.net/wp-content/uploads/2015/08/Minion.jpg";
-//     }
-// }
-//
-// function get_info(){
-//     let users = new user(document.getElementById("f_name").value , document.getElementById("l_name").value , document.getElementById("password").value,
-//         document.getElementById("email").value , document.getElementById("phone_number").value ,
-//     );
-//     var el = document.querySelector('form');
-//     var matches = el.querySelectorAll('input[name]');
-//     for (var m = 0; m < users.length; m++){
-//         var user_arr = users[m];
-//     }
-//     document.write(`
-//            <table>
-//                 <tr>
-//
-//                 </tr>
-//            </table>
-//     `)
-// }
+/* CLASSES IN JAVASCRIPT EXAMPLE USE */
+class user{
+    constructor(f_name , l_name , password, email , phone_number){
+        this.f_name = f_name;
+        this.l_name = l_name;
+        this.password = password;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.avatar = "http://wicklownews.net/wp-content/uploads/2015/08/Minion.jpg";
+    }
+}
+
+function get_info(){
+    let users = new user(document.getElementById("f_name").value , document.getElementById("l_name").value , document.getElementById("password").value,
+        document.getElementById("email").value , document.getElementById("phone_number").value ,
+    );
+    var el = document.querySelector('form');
+    var matches = el.querySelectorAll('input[name]');
+    for (var m = 0; m < users.length; m++){
+        var user_arr = users[m];
+    }
+    document.write(`
+           <table>
+                <tr>
+
+                </tr>
+           </table>
+    `)
+}
+
+class developer{
+    constructor(nume , prenume , experience , salary){
+        this.nume = nume;
+        this.prenume = prenume;
+        this.experience = experience;
+        this.salary = salary;
+    }
+    show_info(){
+        document.write(`
+            <table border="1px">
+                <tr>
+                    <td>
+                        Nume
+                    </td>
+                    <td>
+                        ${this.nume}
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Prenume
+                    </td>
+                    <td>
+                        ${this.prenume}
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Experienta
+                    </td>
+                    <td>
+                        ${this.experience}
+                    </td>
+                </tr>
+                <tr>
+                     <td>
+                        Salariu
+                    </td>
+                    <td>
+                        ${this.salary}
+                    </td>
+                </tr>
+            </table>
+        `)
+    }
+}
+
+var dev = new developer(prompt("Introduceti Numele dvs") , prompt("Introduceti Prenumele dvs."),
+            prompt("Introduceti experienta dvs") , prompt("Introduceti salariul dvs."));
+dev.show_info();
 
 
 
-/* CLASSA CARE SE NUMESTE DEVELOPER
-*
-* cu constructor astfel incit obiectele care rezulta din aceasta clasa sa avem proprietatile
-* nume prenume experienta salariu
-*
-* */
+class Image{
+    constructor(url , w, h){
 
-// class developer{
-//     constructor(nume , prenume , experience , salary){
-//         this.nume = nume;
-//         this.prenume = prenume;
-//         this.experience = experience;
-//         this.salary = salary;
-//     }
-//     show_info(){
-//         document.write(`
-//             <table border="1px">
-//                 <tr>
-//                     <td>
-//                         Nume
-//                     </td>
-//                     <td>
-//                         ${this.nume}
-//                     </td>
-//                 </tr>
-//                 <tr>
-//                     <td>
-//                         Prenume
-//                     </td>
-//                     <td>
-//                         ${this.prenume}
-//                     </td>
-//                 </tr>
-//                 <tr>
-//                     <td>
-//                         Experienta
-//                     </td>
-//                     <td>
-//                         ${this.experience}
-//                     </td>
-//                 </tr>
-//                 <tr>
-//                      <td>
-//                         Salariu
-//                     </td>
-//                     <td>
-//                         ${this.salary}
-//                     </td>
-//                 </tr>
-//             </table>
-//         `)
-//     }
-// }
-//
-// var dev = new developer(prompt("Introduceti Numele dvs") , prompt("Introduceti Prenumele dvs."),
-//             prompt("Introduceti experienta dvs") , prompt("Introduceti salariul dvs."));
-// dev.show_info();
+        //////////  Prototype //////
+        this.src = url;
+        this.width = w;
+        this.height = h;
+        //////////////////////////
+    }
+    show(){
+        document.write(
+            `
+                <img src="${this.src} width='${this.width} height='${this.height}'"/>
+            `
+        )
+    }
+    ceva(){
 
+    }
 
-//
-// class Image{
-//     constructor(url , w, h){
-//
-//         //////////  Prototype //////
-//         this.src = url;
-//         this.width = w;
-//         this.height = h;
-//         //////////////////////////
-//     }
-//     show(){
-//         document.write(
-//             `
-//                 <img src="${this.src} width='${this.width} height='${this.height}'"/>
-//             `
-//         )
-//     }
-//     ceva(){
-//
-//     }
-//
-// }
-// /////////// Obiectul real
-// var logo = new Image("https://soyeon12.files.wordpress.com/2013/07/dm2_minion_dave_020.jpg" , 120 , 60);
-// var baner = new Image("https://www.independent.ie/incoming/article34320338.ece/ALTERNATES/h342/minions1.jpg" , 320 , 800);
+}
+/////////// Obiectul real
+var logo = new Image("https://soyeon12.files.wordpress.com/2013/07/dm2_minion_dave_020.jpg" , 120 , 60);
+var baner = new Image("https://www.independent.ie/incoming/article34320338.ece/ALTERNATES/h342/minions1.jpg" , 320 , 800);
 
-// logo.show();
-// baner.show();
+logo.show();
+baner.show();
 
 class Brad{
     constructor(height , specie , provenenta , culoare, virsta, pret){
@@ -189,10 +180,6 @@ var brad_mare = new Brad(prompt("Scrieti inaltimea la al treilea brad") , prompt
     prompt("Scrieti locul in care a fost crescut al treilea brad") , prompt("Scrieti culoarea la al treilea brad") ,
     prompt("Scrieti virsta la al treilea brad") , prompt("Scrieti pretul la al treilea brad"));
 
-
-/* show facem la brad si sa se deseneze bradul prin document.write prin css  si sa fie identic cu parametrii care intra
-*   la crearea obiectului
-* */
 bradulet.show_brad();
 brad.show_brad();
 brad_mare.show_brad();
